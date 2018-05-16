1e0e8293bb67c3f94340142e11d5973f77a8288d
SistemasCAE <sistemas_cae@cae3076.com>
2018-05-16 15:37:29 -0500

    Notificaciones

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,18 @@
+var ArrMenu =[
+	{
+		nombre: "Consulta Pedimentos",
+		url:"inicio"
+	},
+	{
+		nombre: "Consulta Notificaciones",
+		url:"notificaciones"
+	},
+	{
+		nombre: "Configuración",
+		url:"configuracion"
+	}
+]
 var fn = {
-
 	deviceready: function(){
 		document.addEventListener("deviceready", fn.init/*this.init*/, false);
 	},
@@ -8,14 +21,134 @@ var fn = {
 	 * En esta sección vamos a asociar
 	 * todos los eventos del "Click" al HTML
 	 */
+	  if(window.localStorage.getItem("configuracion") == null){
+			window.localStorage.setItem("configuracion","");
+	  }
+	  
+	  fn.Menu();
+	  fn.cargaNotificaciones();
 	  fn.compruebaSesion();
 	  $("#botonAcceder").tap(fn.iniciarSesion);
+	  $("#botonGuardaConfig").tap(fn.inicioRegistroCel);
    	  $("#botonConsultarPedimento").tap(fn.consultaPedimento);
 	  $("#botonConsultarFecha").tap(fn.consultaFechaPago);
 	  $("#linkConsultaNoPedimento").tap(fn.divPorPedimento);
 	  $("#linkConsultaFechaPago").tap(fn.divPorFechaPago);
 	  $("#cierraSesion").tap(fn.cierraSesion);
+	  $("#cierraSesion2").tap(fn.cierraSesion);
+	  $("#cierraSesion3").tap(fn.cierraSesion);
 	  
+	},
+	cargaNotificaciones : function(){
+		var frecuenciaNotificaciones = window.localStorage.getItem("frecuenciaNotifica");
+		if(window.localStorage.getItem("switchNotifica") != null){
+			var tiempo = new Date();
+			var hora = tiempo.getHours();
+			if(hora >= '08' && hora <= '23')
+			{
+				var idDispositivo = window.localStorage.getItem("registrationID");
+				////////////////////////////////////////////////////////////// Envio AJAX//////////////////////////////////////////////////////////////////
+				$.ajax({
+					type: "GET",
+					url: "http://enlinea.laser-oe.com.mx/AppConsultaPedimentos/Notificaciones/funciones2.php",
+					data: { 
+						datos : idDispositivo
+					},
+					dataType: "json"
+				}).done(function(data, textStatus, jqXHR){
+					for(var x=0; x<data.length; x++)
+					{
+						$('#contenidoNotificaciones').append('<li><a href="#" data-role="button" data-icon="alert" data-theme="a">'+data[x].Mensaje+'</a></li>');
+					}
+				}).fail(function(error){
+
+				});
+				///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			}
+			else{
+			}
+		}
+	},
+	inicioRegistroCel : function(){
+		plataforma=device.platform;
+        fn.setupPush();
+		$('#popup1').html('<center><img src="img/loading3.png" alt="" width="200"></center>');
+		$("#popup1").popup("open");
+	},
+	setupPush: function() {
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "816833643158"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            	},
+            "windows": {}
+        });
+		
+        push.on('registration', function(data) {
+		
+		window.localStorage.setItem("switchNotifica", $("#switchNotificaciones").val());
+		window.localStorage.setItem("frecuenciaNotifica", $("#rango").val());
+		window.localStorage.setItem("registrationID", data.registrationId);
+		
+		jQuery.ajax({
+			url: 'http://enlinea.laser-oe.com.mx/AppConsultaPedimentos/Notificaciones/funciones.php',
+			type:'GET',
+			data:'datos='+data.registrationId+'||'+plataforma+'||'+window.localStorage.getItem("switchNotifica")+'||'+window.localStorage.getItem("frecuenciaNotifica")+'||'+window.localStorage.getItem("nombreUsuario")+'||'+window.localStorage.getItem("aduana"),
+			dataType:'json',
+			success:function(response){
+			  if (response.msg=='primera'){
+				alert('Se ha guardado su configuración');
+				window.localStorage.setItem("configuracion","guardada");
+				$('#popup1').html('');
+				$("#popup1").popup("close");
+			  }else{
+				alert('Se ha actualizado su configuración');
+				$('#popup1').html('');
+				$("#popup1").popup("close");
+			  }
+			},
+			error:function(xhr, status){
+			  alert(status, 'ERROR');
+
+			}
+		  });
+		 
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+
+        });
+		
+        push.on('notification', function(data) {
+    	cordova.plugins.notification.badge.set(0);
+            navigator.notification.alert(
+                data.message,         					// message
+		        "null",          						// callback
+                data.title,           					// title
+                'Ok'                   					// buttonName
+            );
+       });
+    },
+	Menu : function()
+	{
+		var tamArreglo=ArrMenu.length;
+		for(var i = 0; i<tamArreglo; i++)
+		{
+			$("#listaPanel").append('<li><a href="#'+ArrMenu[i].url+'" data-transition="flow" id="0'+ArrMenu[i].url+i+'">'+ArrMenu[i].nombre+'</a></li>');
+			$("#listaPanel2").append('<li><a href="#'+ArrMenu[i].url+'" data-transition="flow" id="1'+ArrMenu[i].url+i+'">'+ArrMenu[i].nombre+'</a></li>');
+			$("#listaPanel3").append('<li><a href="#'+ArrMenu[i].url+'" data-transition="flow" id="2'+ArrMenu[i].url+i+'">'+ArrMenu[i].nombre+'</a></li>');
+		}
 	},
 	iniciarSesion: function(){
 		var usuario = $("#usuario").val();
